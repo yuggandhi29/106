ea561789cab601acba6a02ca7f842750e79c155b
yuggandhi29 <86921687+yuggandhi29@users.noreply.github.com>
2023-05-23 10:28:58 +0530

    Add files via upload

---
+++ Walkers.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+# Create our body classifier
+
+
+# Initiate video capture for video file
+cap = cv2.VideoCapture('walking.avi')
+
+# Loop once video is successfully loaded
+while True:
+    
+    # Read first frame
+    ret, frame = cap.read()
+
+    #Convert Each Frame into Grayscale
+    
+    # Pass frame to our body classifier
+    
+    
+    # Extract bounding boxes for any bodies identified
+    
+
+    if cv2.waitKey(1) == 32: #32 is the Space Key
+        break
+
+cap.release()
+cv2.destroyAllWindows()
